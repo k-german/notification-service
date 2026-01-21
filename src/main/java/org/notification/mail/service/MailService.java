3d3603a464f08849d1cbcd48321cdcbc9afe1fbb
german_kiselev <g.a.kiselev@gmail.com>
2026-01-21 15:25:42 +0300

    MailService.java added application.properties: SMTP template config added

---
+++ src/main/java/org/notification/mail/service/MailService.java
@@ -0,0 +1,35 @@
+package org.notification.mail.service;
+
+import org.notification.kafka.dto.OperationType;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${notification.mail.from}")
+    private String from;
+
+    public MailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendNotification(String email, OperationType operation) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(email);
+        message.setSubject("Test message");
+
+        if (operation == OperationType.CREATE) {
+            message.setText("Hello! Your account has been successfully created.");
+        } else if (operation == OperationType.DELETE) {
+            message.setText("Hello! Your account has been deleted.");
+        }
+
+        mailSender.send(message);
+    }
+}
