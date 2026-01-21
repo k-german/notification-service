db4c359c21b59834ff81219e37d2268713cb37e6
german_kiselev <g.a.kiselev@gmail.com>
2026-01-21 13:25:16 +0300

    init

---
+++ src/main/java/ogr/hiber/App.java
@@ -0,0 +1,13 @@
+package ogr.hiber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
